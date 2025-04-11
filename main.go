a6e9ae9f61e5ed4a075a7d01dc1505c26b795fb6
wood <wadii.zaim@kosmo.delivery>
2025-04-11 18:10:27 +0200

    demo: added-custom-http-resty-clients-with-loggers

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/Flagsmith/flagsmith-go-client/v4"
+	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +22,8 @@ type Flag struct {
 	Enabled bool        `json:"enabled"`
 	Value   interface{} `json:"value"`
 }
+
+type CustomRoundTripper struct{}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	const exampleFlag = "my_go_flag"
@@ -47,6 +53,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	_ = godotenv.Load()
+	rt := CustomRoundTripper{}
+
+	customHttpClient := &http.Client{
+		Transport: rt,
+	}
+
+	fmt.Println(customHttpClient.Transport)
+
+	restyClient := resty.New()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	restyClient.OnBeforeRequest(func(c *resty.Client, req *resty.Request) error {
+		logger.Info("HTTP request",
+			slog.String("method", req.Method),
+			slog.String("url", req.URL),
+			slog.Any("headers", req.Header),
+		)
+		return nil
+	})
+
+	restyClient.OnAfterResponse(func(c *resty.Client, resp *resty.Response) error {
+		logger.Info("HTTP response",
+			slog.String("url", resp.Request.URL),
+			slog.Int("status", resp.StatusCode()),
+			slog.Int("bytes", len(resp.Body())),
+			slog.Duration("elapsed", resp.Time()),
+		)
+		return nil
+	})
 
 	apiKey := os.Getenv("FLAGSMITH_API_KEY")
 	// baseURL := os.Getenv("FLAGSMITH_BASE_URL")
@@ -54,9 +88,31 @@ func main() {
 		log.Fatal("FLAGSMITH_API_KEY not set")
 	}
 
-	client = flagsmith.NewClient(apiKey)
+	client = flagsmith.NewClient(apiKey, flagsmith.WithRestyClient(restyClient))
 
 	http.HandleFunc("/", handler)
 	log.Println("Listening on :8082")
 	log.Fatal(http.ListenAndServe(":8082", nil))
+}
+
+func (ct CustomRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("url: %s | method: %s", resp.Request.URL.String(), resp.Request.Method)
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	_ = resp.Body.Close()
+
+	log.Println("number of bytes in tripper:", len(data))
+	log.Printf("value in tripper: %s\n", string(data))
+
+	resp.Body = io.NopCloser(bytes.NewReader(data))
+
+	return resp, nil
 }
